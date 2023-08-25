2203b4543117f8d1a52d02f5319bd0b78c8315f8
dmtrack <dmtrack.dev@gmail.com>
2023-08-25 12:10:48 +0500

    feat/server is started

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+// import connection from './db/config';
+import dotenv from 'dotenv';
+// import fileRouter from './routes/file.routes';
+import { urlencoded, json } from 'body-parser';
+// import handleError from './middleware/error-middleware';
+import cors from 'cors';
+import http from 'http';
+
+export const app = express();
+export const server = http.createServer(app);
+
+dotenv.config();
+app.options(
+    '*',
+    cors({ origin: process.env.CLIENT_URL, optionsSuccessStatus: 200 })
+);
+app.use(cors({ origin: process.env.CLIENT_URL, optionsSuccessStatus: 200 }));
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+// app.use('/file', fileRouter);
+// app.use(handleError);
+
+// connection
+//     .sync({ alter: true })
+//     .then(async () => {
+//         console.log('Database synced successfully, lets go!');
+//     })
+//     .catch((err) => {
+//         console.log('Err', err);
+//     });
