e1cbb10507b3d18ccd8cc2c40d39f166b48aa51f
dmtrack <dmtrack.dev@gmail.com>
2023-08-25 22:07:13 +0500

    feat/schedule request added

--- src/controllers/fileController.ts
+++ src/controllers/fileController.ts
@@ -4,6 +4,7 @@ import fetchService from '../services/fetch.service.js';
 import compressService from '../services/compress.service.js';
 import dbService from '../services/db.service.js';
 import convertService from '../services/convert.service.js';
+import { NextFunction } from 'express-serve-static-core';
 
 class FileController {
     downloadBeakData: RequestHandler = async (req, res, next) => {
@@ -11,11 +12,16 @@ class FileController {
             await fetchService.downloadBeakData();
             await compressService.unzipFiles();
             const bankCards = await convertService.xmlToJson();
-
-            res.json(bankCards);
+            console.log(bankCards, new Date().toUTCString());
         } catch (e) {
             next(e);
         }
+    };
+
+    downloadBeakDataSchedule: RequestHandler = (req, res, next) => {
+        setInterval(() => {
+            this.downloadBeakData(req, res, next);
+        }, 3000);
     };
 }
 
--- src/routes/file.routes.ts
+++ src/routes/file.routes.ts
@@ -4,6 +4,7 @@ import fileController from '../controllers/fileController.js';
 const fileRouter = Router();
 
 fileRouter.get('/getbeak', fileController.downloadBeakData);
+fileRouter.get('/getbeakschedule', fileController.downloadBeakDataSchedule);
 
 export default fileRouter;
 
